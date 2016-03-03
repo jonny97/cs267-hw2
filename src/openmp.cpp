b76f0b69c447ea4ce9f26f1e364c978cb1adaa18
geohussain <geohussain@berkeley.edu>
2016-03-03 11:51:41 -0800

    editted report v2

--- src/openmp.cpp
+++ src/openmp.cpp
@@ -2,129 +2,564 @@
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
-#include "common.h"
 #include "omp.h"
+#include <vector>
+#include <float.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
 
+//            //
+//  common.h  //
+//            //
+
+//
+//  tuned constants
+//
+#define density 0.0005
+#define mass    0.01
+#define cutoff  0.01
+#define min_r   (cutoff/100)
+#define dt      0.0005
+#define bin_size (cutoff*1)
+
+#ifndef __CS267_COMMON_H__
+#define __CS267_COMMON_H__
+
+inline int mymin( int a, int b ) { return a < b ? a : b; }
+inline int mymax( int a, int b ) { return a > b ? a : b; }
+//
+//  saving parameters
+//
+const int NSTEPS = 1000;
+const int SAVEFREQ = 10;
+
+//
+// particle data structure
+//
+typedef struct 
+{
+  double x;
+  double y;
+  double vx;
+  double vy;
+  double ax;
+  double ay;
+} particle_t;
+
+
+class bin_t
+{
+private:
+  std::vector<particle_t *> particles;
+public:
+  bin_t() { particles.resize(100); }
+  void add_particle(particle_t *particle) {
+    particles.push_back(particle);
+  }
+  void clear() {
+    particles.clear();
+  }
+  particle_t *get_particle(int i) {
+    return particles[i];
+  }
+  int num_particles() {
+    return particles.size();
+  }
+};
+
+//
+//  timing routines
+//
+double read_timer2( );
+
+//
+//  simulation routines
+//
+void set_size2( int n );
+void init_particles2( int n, particle_t *p );
+int init_bins (bin_t*& bins);
+void clear_bins(bin_t*& bins);
+void bin_particles( bin_t *bins, particle_t *particles, int n);
+void bin_particles( bin_t *bins, particle_t *particles, int *particle_bin_ids, int n);
+void apply_force( particle_t &particle, particle_t &neighbor , double *dmin, double &davg, int &navg);
+void apply_force_particle_bin( particle_t &particle, int bin_id, bin_t *bins, double *dmin, double &davg, int &navg);
+void apply_force_bin( bin_t *bins, int bin_id, double *dmin, double &davg, int &navg);
+void move( particle_t &p );
+
+
+//
+//  I/O routines
+//
+FILE *open_save( char *filename, int n );
+void save2( FILE *f, int n, particle_t *p );
+void save2( FILE *f, bin_t *bins );
+
+//
+//  argument processing routines
+//
+int find_option2( int argc, char **argv, const char *option );
+int read_int2( int argc, char **argv, const char *option, int default_value );
+char *read_string2( int argc, char **argv, const char *option, char *default_value );
+
+#endif
+
+//              //
+//  common.cpp  //
+//              //
+
+double size2;
+int n_bins_per_side;
+
+//
+//  timer
+//
+double read_timer2( )
+{
+    static bool initialized = false;
+    static struct timeval start;
+    struct timeval end;
+    if( !initialized )
+    {
+        gettimeofday( &start, NULL );
+        initialized = true;
+    }
+    gettimeofday( &end, NULL );
+    return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
+}
+
+//
+//  keep density constant
+//
+void set_size2( int n )
+{
+    size2 = sqrt( density * n );
+}
+
+//
+//  Initialize the particle positions and velocities
+//
+void init_particles2( int n, particle_t *p )
+{
+//  we force same randomness for now
+//    srand48( time( NULL ) );
+    srand48(0);
+        
+    int sx = (int)ceil(sqrt((double)n));
+    int sy = (n+sx-1)/sx;
+    
+    int *shuffle = (int*)malloc( n * sizeof(int) );
+    for( int i = 0; i < n; i++ )
+        shuffle[i] = i;
+    
+    for( int i = 0; i < n; i++ ) 
+    {
+        //
+        //  make sure particles are not spatially sorted
+        //
+        int j = lrand48()%(n-i);
+        int k = shuffle[j];
+        shuffle[j] = shuffle[n-i-1];
+        
+        //
+        //  distribute particles evenly to ensure proper spacing
+        //
+        p[i].x = size2*(1.+(k%sx))/(1+sx);
+        p[i].y = size2*(1.+(k/sx))/(1+sy);
+
+        //
+        //  assign random velocities within a bound
+        //
+        p[i].vx = drand48()*2-1;
+        p[i].vy = drand48()*2-1;
+    }
+    free( shuffle );
+}
+
+//
+// Allocate bins
+//
+int init_bins (bin_t*& bins) {
+  n_bins_per_side = ceil(size2 / bin_size);
+  int n_bins = n_bins_per_side * n_bins_per_side;
+  bins = new bin_t[n_bins];
+  return n_bins;
+}
+
+void clear_bins(bin_t*& bins) {
+  delete [] bins;
+}
+
+//
+// Assign particles to bins
+//
+void bin_particles( bin_t *bins, particle_t *particles, int n) {
+  for (int i = 0; i < n_bins_per_side; i++) {
+    for (int j = 0; j < n_bins_per_side; j++) {
+      bins[i*n_bins_per_side+j].clear();
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    int bin_x_id = floor(particles[i].x / bin_size);
+    int bin_y_id = floor(particles[i].y / bin_size);
+    int bin_id = bin_x_id * n_bins_per_side + bin_y_id;
+    bins[bin_id].add_particle(particles+i);
+  }
+}
+
+void bin_particles( bin_t *bins, particle_t *particles, int *particle_bin_ids, int n) {
+  for (int i = 0; i < n_bins_per_side; i++) {
+    for (int j = 0; j < n_bins_per_side; j++) {
+      bins[i*n_bins_per_side+j].clear();
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    int bin_x_id = floor(particles[i].x / bin_size);
+    int bin_y_id = floor(particles[i].y / bin_size);
+    int bin_id = bin_x_id * n_bins_per_side + bin_y_id;
+    particle_bin_ids[i] = bin_id;
+    bins[bin_id].add_particle(particles+i);
+  }
+}
+//
+//  interact two particles
+//
+void apply_force( particle_t &particle, particle_t &neighbor , double *dmin, double &davg, int &navg)
+{
+
+    double dx = neighbor.x - particle.x;
+    double dy = neighbor.y - particle.y;
+    double r2 = dx * dx + dy * dy;
+    if( r2 > cutoff*cutoff )
+        return;
+    if (dmin != NULL && r2 != 0)
+    {
+       if (r2/(cutoff*cutoff) < *dmin * (*dmin))
+          *dmin = sqrt(r2)/cutoff;
+       davg += sqrt(r2)/cutoff;
+       navg ++;
+    }
+        
+    r2 = fmax( r2, min_r*min_r );
+    double r = sqrt( r2 );
+    
+    //
+    //  very simple short-range repulsive force
+    //
+    double coef = ( 1 - cutoff / r ) / r2 / mass;
+    particle.ax += coef * dx;
+    particle.ay += coef * dy;
+}
+
+void apply_force_bin( bin_t *bins, int bin_id, double *dmin, double &davg, int &navg) {
+  if (bins[bin_id].num_particles() == 0) // nothing to apply
+    return;
+
+  for (int i = 0; i < bins[bin_id].num_particles(); i++) {
+    apply_force_particle_bin( *(bins[bin_id].get_particle(i)), bin_id, bins, dmin, davg, navg);
+  }
+}
+
+void apply_force_particle_bin( particle_t &particle, int bin_id, bin_t *bins, double *dmin, double &davg, int &navg) {
+    int bin_x_id = bin_id / n_bins_per_side;
+    int bin_y_id = bin_id % n_bins_per_side;
+
+    // reset acceleration
+    particle.ax = particle.ay = 0;
+
+    // apply force within bin
+    for (int j = 0; j < bins[bin_id].num_particles(); j++) {
+      apply_force(particle, *(bins[bin_id].get_particle(j)), dmin, davg, navg);
+    }
+
+
+    // apply force from edge-neighboring bins
+    if (bin_x_id != 0) {
+      int neighbor_id = bin_id - n_bins_per_side;
+      for (int j = 0; j < bins[neighbor_id].num_particles(); j++) {
+        apply_force(particle, *(bins[neighbor_id].get_particle(j)), dmin, davg, navg);
+      }
+    }
+    if (bin_x_id != n_bins_per_side-1) {
+      int neighbor_id = bin_id + n_bins_per_side;
+      for (int j = 0; j < bins[neighbor_id].num_particles(); j++) {
+        apply_force(particle, *(bins[neighbor_id].get_particle(j)), dmin, davg, navg);
+      }
+    }
+    if (bin_y_id != 0) {
+      int neighbor_id = bin_id - 1;
+      for (int j = 0; j < bins[neighbor_id].num_particles(); j++) {
+        apply_force(particle, *(bins[neighbor_id].get_particle(j)), dmin, davg, navg);
+      }
+    }
+    if (bin_y_id != n_bins_per_side-1) {
+      int neighbor_id = bin_id + 1;
+      for (int j = 0; j < bins[neighbor_id].num_particles(); j++) {
+        apply_force(particle, *(bins[neighbor_id].get_particle(j)), dmin, davg, navg);
+      }
+    }
+
+    // apply force from edge-neighboring bins
+    if (bin_x_id != 0 && bin_y_id != 0) {
+      int neighbor_id = bin_id - (n_bins_per_side + 1);
+      for (int j = 0; j < bins[neighbor_id].num_particles(); j++) {
+        apply_force(particle, *(bins[neighbor_id].get_particle(j)), dmin, davg, navg);
+      }
+    }
+    if (bin_x_id != 0 && bin_y_id != n_bins_per_side-1) {
+      int neighbor_id = bin_id - (n_bins_per_side - 1);
+      for (int j = 0; j < bins[neighbor_id].num_particles(); j++) {
+        apply_force(particle, *(bins[neighbor_id].get_particle(j)), dmin, davg, navg);
+      }
+    }
+    if (bin_x_id != n_bins_per_side-1 && bin_y_id != 0) {
+      int neighbor_id = bin_id + n_bins_per_side - 1;
+      for (int j = 0; j < bins[neighbor_id].num_particles(); j++) {
+        apply_force(particle, *(bins[neighbor_id].get_particle(j)), dmin, davg, navg);
+      }
+    }
+    if (bin_x_id != n_bins_per_side-1 && bin_y_id != n_bins_per_side-1) {
+      int neighbor_id = bin_id + n_bins_per_side + 1;
+      for (int j = 0; j < bins[neighbor_id].num_particles(); j++) {
+        apply_force(particle, *(bins[neighbor_id].get_particle(j)), dmin, davg, navg);
+      }
+    }
+}
+
+
+//
+//  integrate the ODE
+//
+void move2( particle_t &p )
+{
+    //
+    //  slightly simplified Velocity Verlet integration
+    //  conserves energy better than explicit Euler method
+    //
+    p.vx += p.ax * dt;
+    p.vy += p.ay * dt;
+    p.x  += p.vx * dt;
+    p.y  += p.vy * dt;
+
+    //
+    //  bounce from walls
+    //
+    while( p.x < 0 || p.x > size2 )
+    {
+        p.x  = p.x < 0 ? -p.x : 2*size2-p.x;
+        p.vx = -p.vx;
+    }
+    while( p.y < 0 || p.y > size2 )
+    {
+        p.y  = p.y < 0 ? -p.y : 2*size2-p.y;
+        p.vy = -p.vy;
+    }
+}
+
+//
+//  I/O routines
+//
+void save2( FILE *f, int n, particle_t *p )
+{
+    static bool first = true;
+    if( first )
+    {
+        fprintf( f, "%d %g\n", n, size2 );
+        first = false;
+    }
+    for( int i = 0; i < n; i++ )
+        fprintf( f, "%g %g\n", p[i].x, p[i].y );
+}
+
+//
+//  command line option processing
+//
+int find_option2( int argc, char **argv, const char *option )
+{
+    for( int i = 1; i < argc; i++ )
+        if( strcmp( argv[i], option ) == 0 )
+            return i;
+    return -1;
+}
+
+int read_int2( int argc, char **argv, const char *option, int default_value )
+{
+    int iplace = find_option2( argc, argv, option );
+    if( iplace >= 0 && iplace < argc-1 )
+        return atoi( argv[iplace+1] );
+    return default_value;
+}
+
+char *read_string2( int argc, char **argv, const char *option, char *default_value )
+{
+    int iplace = find_option2( argc, argv, option );
+    if( iplace >= 0 && iplace < argc-1 )
+        return argv[iplace+1];
+    return default_value;
+}
+
+//              //
+//  openmp.cpp  //
+//               //
+
+
+///////////////////////////
 //
 //  benchmarking program
 //
 int main( int argc, char **argv )
 {   
-    int navg,nabsavg=0,numthreads; 
-    double dmin, absmin=1.0,davg,absavg=0.0;
-	
-    if( find_option( argc, argv, "-h" ) >= 0 )
-    {
-        printf( "Options:\n" );
-        printf( "-h to see this help\n" );
-        printf( "-n <int> to set number of particles\n" );
-        printf( "-o <filename> to specify the output file name\n" );
-        printf( "-s <filename> to specify a summary file name\n" ); 
-        printf( "-no turns off all correctness checks and particle output\n");   
-        return 0;
-    }
+  int navg,nabsavg=0,numthreads; 
+  double dmin, absmin=1.0,davg,absavg=0.0;
+  
+  if( find_option2( argc, argv, "-h" ) >= 0 )
+  {
+    printf( "Options:\n" );
+    printf( "-h to see this help\n" );
+    printf( "-n <int> to set number of particles\n" );
+    printf( "-o <filename> to specify the output file name\n" );
+    printf( "-s <filename> to specify a summary file name\n" );
+    printf( "-p <int> to set number of threads to use\n");
+    printf( "-no turns off all correctness checks and particle output\n");   
+    return 0;
+  }
 
-    int n = read_int( argc, argv, "-n", 1000 );
-    char *savename = read_string( argc, argv, "-o", NULL );
-    char *sumname = read_string( argc, argv, "-s", NULL );
+  int n = read_int2( argc, argv, "-n", 1000 );
+  char *savename = read_string2( argc, argv, "-o", NULL );
+  char *sumname = read_string2( argc, argv, "-s", NULL );
 
-    FILE *fsave = savename ? fopen( savename, "w" ) : NULL;
-    FILE *fsum = sumname ? fopen ( sumname, "a" ) : NULL;      
+  FILE *fsave = savename ? fopen( savename, "w" ) : NULL;
+  FILE *fsum = sumname ? fopen ( sumname, "a" ) : NULL;      
 
-    particle_t *particles = (particle_t*) malloc( n * sizeof(particle_t) );
-    set_size( n );
-    init_particles( n, particles );
+  int p = read_int2( argc, argv, "-p", 1 );
+  omp_set_num_threads(p);
 
-    //
-    //  simulate a number of time steps
-    //
-    double simulation_time = read_timer( );
+  particle_t *particles = (particle_t*) malloc( n * sizeof(particle_t) );
+  set_size2( n );
+  init_particles2( n, particles );
 
-    #pragma omp parallel private(dmin) 
-    {
+  bin_t *bins;
+  int n_bins = init_bins(bins);
+  int n_bins_per_side = sqrt(n_bins);
+  omp_lock_t* locks = (omp_lock_t*) malloc(n_bins * sizeof(omp_lock_t) );
+  for (int i = 0; i < n_bins; i++) {
+    omp_init_lock(locks+i);
+  }
+
+  //
+  //  simulate a number of time steps
+  //
+  double simulation_time = read_timer2( );
+
+
+  #pragma omp parallel private(dmin)
+  {
     numthreads = omp_get_num_threads();
+
     for( int step = 0; step < NSTEPS; step++ )
     {
-        navg = 0;
-        davg = 0.0;
-	dmin = 1.0;
-        //
-        //  compute all forces
-        //
+      navg = 0;
+      davg = 0.0;
+      dmin = 1.0;
+
+      //  assign particles to bins
+      #pragma omp for
+      for (int i = 0; i < n_bins_per_side; i++) {
+        for (int j = 0; j < n_bins_per_side; j++) {
+          bins[i*n_bins_per_side+j].clear();
+        }
+      }
+
+      #pragma omp for 
+      for (int i = 0; i < n; i++) {
+        int bin_x_id = floor(particles[i].x / bin_size);
+        int bin_y_id = floor(particles[i].y / bin_size);
+        int bin_id = bin_x_id * n_bins_per_side + bin_y_id;
+        omp_set_lock(locks+bin_id);
+        bins[bin_id].add_particle(particles+i);
+        omp_unset_lock(locks+bin_id);
+      }
+ 
+      //
+      //  compute all forces
+      //
+      if( find_option2( argc, argv, "-no" ) == -1 ) {
         #pragma omp for reduction (+:navg) reduction(+:davg)
-        for( int i = 0; i < n; i++ )
-        {
-            particles[i].ax = particles[i].ay = 0;
-            for (int j = 0; j < n; j++ )
-                apply_force( particles[i], particles[j],&dmin,&davg,&navg);
+        for (int i = 0; i < n_bins; i++) {
+          apply_force_bin (bins, i, &dmin,davg,navg);
         }
-        
-		
-        //
-        //  move particles
-        //
+      } else {
         #pragma omp for
-        for( int i = 0; i < n; i++ ) 
-            move( particles[i] );
-  
-        if( find_option( argc, argv, "-no" ) == -1 ) 
-        {
-          //
-          //  compute statistical data
-          //
-          #pragma omp master
-          if (navg) { 
-            absavg += davg/navg;
-            nabsavg++;
-          }
-
-          #pragma omp critical
-	  if (dmin < absmin) absmin = dmin; 
-		
-          //
-          //  save if necessary
-          //
-          #pragma omp master
-          if( fsave && (step%SAVEFREQ) == 0 )
-              save( fsave, n, particles );
+        for (int i = 0; i < n_bins; i++) {
+          apply_force_bin (bins, i, NULL, davg, navg);
         }
-    }
-}
-    simulation_time = read_timer( ) - simulation_time;
-    
-    printf( "n = %d,threads = %d, simulation time = %g seconds", n,numthreads, simulation_time);
+      }
 
-    if( find_option( argc, argv, "-no" ) == -1 )
-    {
-      if (nabsavg) absavg /= nabsavg;
+      //
+      //  move particles
+      //
+      #pragma omp for
+      for( int i = 0; i < n; i++ ) 
+        move2( particles[i] );
+
+      if( find_option2( argc, argv, "-no" ) == -1 ) 
+      {
+        //
+        //  compute statistical data
+        //
+        if (navg) { 
+          absavg += davg/navg;
+          nabsavg++;
+        }
+
+        #pragma omp critical
+        if (dmin < absmin) absmin = dmin; 
+      
+        //
+        //  save if necessary
+        //
+        if( fsave && (step%SAVEFREQ) == 0 )
+          save2( fsave, n, particles );
+      }
+    }
+  }
+
+  simulation_time = read_timer2( ) - simulation_time;
+
+  printf( "n = %d,threads = %d, simulation time = %g seconds", n,numthreads, simulation_time);
+
+  if( find_option2( argc, argv, "-no" ) == -1 )
+  {
+    if (nabsavg) absavg /= nabsavg;
     // 
-    //  -The minimum distance absmin between 2 particles during the run of the simulation
+    //  -the minimum distance absmin between 2 particles during the run of the simulation
     //  -A Correct simulation will have particles stay at greater than 0.4 (of cutoff) with typical values between .7-.8
-    //  -A simulation where particles don't interact correctly will be less than 0.4 (of cutoff) with typical values between .01-.05
+    //  -A simulation were particles don't interact correctly will be less than 0.4 (of cutoff) with typical values between .01-.05
     //
     //  -The average distance absavg is ~.95 when most particles are interacting correctly and ~.66 when no particles are interacting
     //
     printf( ", absmin = %lf, absavg = %lf", absmin, absavg);
     if (absmin < 0.4) printf ("\nThe minimum distance is below 0.4 meaning that some particle is not interacting");
     if (absavg < 0.8) printf ("\nThe average distance is below 0.8 meaning that most particles are not interacting");
-    }
-    printf("\n");
-    
-    //
-    // Printing summary data
-    //
-    if( fsum)
-        fprintf(fsum,"%d %d %g\n",n,numthreads,simulation_time);
+  }
+  printf("\n");
 
-    //
-    // Clearing space
-    //
-    if( fsum )
-        fclose( fsum );
+  //
+  // Printing summary data
+  //
+  if( fsum)
+    fprintf(fsum,"%d %d %g\n",n,numthreads,simulation_time);
 
-    free( particles );
-    if( fsave )
-        fclose( fsave );
-    
-    return 0;
+  //
+  // Clearing space
+  //
+  if( fsum )
+    fclose( fsum );
+
+  clear_bins(bins);
+  free( particles );
+  if( fsave )
+    fclose( fsave );
+
+  return 0;
 }
